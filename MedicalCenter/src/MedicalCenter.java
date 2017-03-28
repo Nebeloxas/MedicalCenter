ff112264f4f2121db1608edecff3d4c771c6fd45
iBadMan_PC <iBadMan_PC@DESKTOP-EESAVRU>
2017-03-28 20:52:25 +0300

    1

--- MedicalCenter/src/MedicalCenter.java
+++ MedicalCenter/src/MedicalCenter.java
@@ -1,5 +1,13 @@
+import java.awt.Canvas;
 
-public class MedicalCenter {
+
+public class MedicalCenter extends Canvas {
+
+	private static final long serialVersionUID = 1L;
+	public static final int WIDTH = 10 * 32;
+	public static final int HEIGHT = WIDTH;
+	public static final int SCALE = 2;
+	public final String TITLE = "Medical Center Application";
 
 	public static void main(String[] args) {
 		//asdasda
