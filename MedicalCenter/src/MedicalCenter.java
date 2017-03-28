c7498b85de9bde447dda6fb1f2e45b97412d5d22
iBadMan_PC <iBadMan_PC@DESKTOP-EESAVRU>
2017-03-28 21:03:23 +0300

    555

--- MedicalCenter/src/MedicalCenter.java
+++ MedicalCenter/src/MedicalCenter.java
@@ -9,7 +9,7 @@ public class MedicalCenter extends Canvas {
 	public static final int SCALE = 2;
 	public final String TITLE = "Medical Center Application";
 	
-	//asdasd
+	//asdasdmjknijkbjkbbsad asd 
 
 	public static void main(String[] args) {
 		//asdasda
